1de73f2151809dbf06e0e6cc842b509fbb7c214c
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2014-10-04 19:55:30 +0200

    Version 0.00
    Added logo.ascii
    Now it will setup the keys
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This is a installer for Jetspace ;-)
 ##What is Jetspace?
 
 Jetspace is a after-install kit for Linux. You just run this after your setup, and you're done!
+
+
+
+
+
+#IMPORTANT NOTE:
+
+This Software comes WITHOUT ANY KIND OF WARRENTY! YOU USE IT ON YOUR OWN RISK!
--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,52 @@
+#!/bin/bash
+
+KeyboardLayout()
+{
+  clear
+  echo "Choose Keyboard Layout: (if you enter none or a false value, it will use US keys)"
+  echo " 1> German  QWERTZ"
+  echo " 2> English QWERTY"
+
+  read $BUFFER -n 1
+
+  if [ "$BUFFER" == "1" ]
+  then
+  KEYBORAD="loadkeys de-latin1"
+  elif [ "$BUFFER" == "2" ]
+  then
+  KEYBORAD="loadkeys us"
+  else
+  KEYBOARD="loadkeys us"
+  fi
+  loadkeys $KEYBOARD
+
+}
+
+
+
 clear
-   __  ______ ______ ______  ______ ______  ______  ______
-  /\ \/\  ___/\__  _/\  ___\/\  == /\  __ \/\  ___\/\  ___\   
- _\_\ \ \  __\/_/\ \\ \___  \ \  _-\ \  __ \ \ \___\ \  __\   
-/\_____\ \_____\\ \_\\/\_____\ \_\  \ \_\ \_\ \_____\ \_____\ 
-\/_____/\/_____/ \/_/ \/_____/\/_/   \/_/\/_/\/_____/\/_____/ 
-                                                              
+cat src/logo.ascii
+echo "A free Arch Based Distro"
+echo "------------------------"
+echo -e "\n\n" # New Lines
+
+#Now tell the user how the installation will look like
+echo "Installation Plan:"
+echo " > Choose Keyboard Layout"
+echo " > Part the Disk"
+echo " > Mount Patitions"
+echo " > Setup Internet"
+echo " > Install BASE ARCH system"
+echo " > Configure System"
+echo " > Bootloaded Setup"
+echo " -----Reboot-----"
+echo " > Install Jetspace Stuff"
+
+echo -e "\nPress [ENTER] to continue"
+read -s
+
+#Setup the Keys
+KeyboardLayout
+
+
+#
